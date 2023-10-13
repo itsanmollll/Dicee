25a9af8455b191663c6e67270fa3c9e93b8cb47f
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-10-13 13:45:35 +0530

    Ending Phase

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,7 +1,4 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
+//  Dicee
 //  Created by Anmol Bhardwaj on 12/10/2023.
 
 
@@ -9,28 +6,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diceeImageView1: UIImageView!
+    // Use of IBOutlet is to show code in Ui so ecvery user interect with it means nothinng change by tapping or doing anything by user
     
-    @IBOutlet weak var diceeImageView2: UIImageView!
+    @IBOutlet weak var diceeImageView1: UIImageView! // This ine means what to display in ui at this place i.e image 1
     
-    var leftDiceNumber = 0
+    @IBOutlet weak var diceeImageView2: UIImageView! // This ine means what to display in ui at this place i.e image 2
     
-    var rightDiceNumber = 5
+//    var leftDiceNumber = 0
+//    
+//    var rightDiceNumber = 5
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
         //diceeImageView1.image = UIImage(imageLiteralResourceName:"DiceSix")
         //diceeImageView1.alpha = 0.5
         //diceeImageView2.image = UIImage(imageLiteralResourceName:"DiceThree")
-    }
+//    }
     
+    
+    // Use of IBAction is to perform action that a attached to this button and perform the function , user can perform action to let it in work 
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //diceeImageView1.image = UIImage(imageLiteralResourceName: "DiceFive")
         //diceeImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
-        diceeImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][leftDiceNumber]
-        leftDiceNumber = leftDiceNumber + 1
-        diceeImageView2.image = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][rightDiceNumber]
-        rightDiceNumber = rightDiceNumber - 1
+        
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        diceeImageView1.image = diceArray.randomElement()/*[Int.random(in: 0...5)]*/
+//        leftDiceNumber += 1
+        diceeImageView2.image = diceArray.randomElement() /* This commented code do the same likr randomElement() do [Int.random(in: 0...5)]*/
+//        rightDiceNumber -= 1
     }
 }
